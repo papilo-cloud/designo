9945259cd5b278e1de25d36017ecde84400023a8
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-10-24 01:34:59 -0700

    added assets file

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-     <h1> Hello world</h1>
-    </>
+    <Routes>
+      <Route  />
+    </Routes>
   )
-}
+} 
 
 export default App
