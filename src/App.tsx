caccc138aebc8f52b76c1a51169b6d0026c8b7d3
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-10-26 11:30:51 -0700

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import { Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { Company } from "./pages/Company";
+import { Contact } from "./pages/Contact";
+import { Location } from "./pages/Location";
+import './index.css'
 
 function App() {
 
   return (
-    <Routes>
-      <Route  />
-    </Routes>
+    <div className="app">
+      <Navbar />
+      <Routes >
+        <Route path="/" element={<Home /> }  />
+        <Route path="/company" element={<Company />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/location" element={<Location />} />
+      </Routes>
+    </div>
   )
 } 
 
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Contact = () => {
+  return (
+    <section>
+        <div  className="container">Contact</div>
+    </section>
+  )
+}
