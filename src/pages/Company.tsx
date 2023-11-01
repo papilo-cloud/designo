e9af4472e9b9a1659204bad0810c566fee58bd2e
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-11-01 11:14:35 -0700

    added the patterns

--- src/pages/Company.tsx
+++ src/pages/Company.tsx
@@ -1,10 +1,12 @@
 import { Hero } from "../components/about/Hero"
+import { Talent } from "../components/about/Talent"
 
 export const Company = () => {
   return (
     <section>
         <div className="container">
           <Hero />
+          <Talent />
         </div>
     </section>
   )
