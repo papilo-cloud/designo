a37ed2be4621cefec107e552b7428a42acebe4fb
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-11-01 18:24:45 -0700

    done with desktop view of the location part

---
+++ src/components/about/Deal.tsx
@@ -0,0 +1,19 @@
+import './about.css'
+
+
+export const Deal = () => {
+  return (
+    <div className='main' data-aos='fade-up' data-aos-duration="1000">
+        <div className="deal">
+            <div className="deal-img"></div>
+            <div className="deal-mesg">
+                <div className="text" data-aos='zoom-in' data-aos-duration="5000">
+                    <h2>The real deal</h2>
+                    <p>As strategic partners in our clients’ businesses, we are ready to take on any challenge as our own. Solving real problems require empathy and collaboration, and we strive to bring a fresh perspective to every opportunity. We make design and technology more accessible and give you tools to measure success.</p>
+                    <p>We are visual storytellers in appealing and captivating ways. By combining business and marketing strategies, we inspire audiences to take action and drive real results.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
