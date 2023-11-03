7c14c70359e1429c6bc1d70ff04d4be98e1d9602
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2023-11-03 11:08:06 -0700

    donw with the contact component

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -3,14 +3,31 @@ import { Location } from "../components/about/Location"
 import { ContactUs } from "../components/contact/ContactUs"
 
 export const Contact = () => {
+
+  const animate = {
+    initial:{
+      opacity: 0, x: -100
+    },
+    anime: {opacity: 1, x: 0},
+    transition: {
+      type: 'tween', ease: 'easeOut' ,duration: 1
+    },
+    exit: {
+      opacity: 0,
+      x: -100,
+      transition: {
+        type: 'tween', ease: 'easeIn' ,duration: 1, staggerChildren: 0
+      }
+    }
+  }
   return (
 
     <AnimatePresence>
     <motion.section
-    initial={{opacity: 0, x: -100}}
-    animate={{opacity: 1, x: 0}}
-    transition={{duration: 0.9}}
-    exit={{x: -100}}
+    variants={animate}
+    initial='initial'
+    animate='anime'
+    exit='exit'
     >
         <div className="container">
           <ContactUs />
--- src/pages/Location.tsx
+++ src/pages/Location.tsx
@@ -1,11 +1,36 @@
+import { AnimatePresence, motion } from 'framer-motion'
 import {Location as Locations} from '../components/location/Location'
 
 export const Location = () => {
+
+  const animate = {
+    initial:{
+      opacity: 0, x: -100
+    },
+    anime: {opacity: 1, x: 0},
+    transition: {
+      type: 'tween', ease: 'easeOut' ,duration: 1
+    },
+    exit: {
+      opacity: 0,
+      x: -100,
+      transition: {
+        type: 'tween', ease: 'easeIn' ,duration: 1, staggerChildren: 0
+      }
+    }
+  }
+
   return (
-    <section>
+    <AnimatePresence>
+    <motion.section
+     variants={animate}
+     initial='initial'
+     animate='anime'
+     exit='exit'>
         <div className="container">
           <Locations />
         </div>
-    </section>
+    </motion.section>
+    </AnimatePresence>
   )
 }
